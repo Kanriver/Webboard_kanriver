ca7531db50cbba9a88fff55b5e621e57c0b1ebfa
Kanriver <Kannatee2546kan@gmail.com>
2024-01-03 15:44:42 +0700

    verify and post in php work 4
    verify and post in php

---
+++ post.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+</head>
+<body>
+    <h1 style="text-align: center;">WebBoard GoodGame</h1>
+    <hr>
+    <div style="text-align: center;">
+        ต้องการดูกระทู้หมายเลข <?php echo $_GET['id']; ?> <br><br>
+        <table style="border: 2px solid black; width: 40%;" align="center">
+            <tr style="background-color: #6CD2FE;"><td colspan="2">แสดงความคิดเห็น</td></tr>
+            <tr><td><textarea name="massage" cols="50" rows="10"></textarea></td></tr>
+            <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+        </table>
+    </div><br>
+    <div style="text-align: center;">
+    <a href="index.html">กลับไปหน้าหลัก</a>
+    </div>
+</body>
+</html>
